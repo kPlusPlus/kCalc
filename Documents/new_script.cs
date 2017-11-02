087132bc7040733b505b06b78bcd01d966bf82fd
kPlusPlus <kresimir.ivkovic@gmail.com>
2017-11-02 22:26:40 +0100

    danas

---
+++ Documents/new_script.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Script
+{
+    String strName = "Pero";
+
+    static void Main(string[] args)
+    {
+        //Console.BackgroundColor = #010101;
+        Int32 a, b;
+        a = 1123;
+        b = 213;
+        Console.WriteLine("Hello, my Angel" + a * b);
+    }
+}
