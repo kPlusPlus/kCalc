efda8cb3aa79c50866de6b0569a2bee972cececf
Kresimir Ivkovic <kresimir.ivkovic@supracontrol.com>
2024-12-21 17:47:03 +0100

    functions color

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace kCalc.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.12.0.0")]
     public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -224,6 +224,30 @@ namespace kCalc.Properties {
             }
             set {
                 this["myScriptBoxBackgroundColor"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("20, 20, 20")]
+        public global::System.Drawing.Color myFuncBackColor {
+            get {
+                return ((global::System.Drawing.Color)(this["myFuncBackColor"]));
+            }
+            set {
+                this["myFuncBackColor"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Yellow")]
+        public global::System.Drawing.Color myFunctFontColor {
+            get {
+                return ((global::System.Drawing.Color)(this["myFunctFontColor"]));
+            }
+            set {
+                this["myFunctFontColor"] = value;
             }
         }
     }
